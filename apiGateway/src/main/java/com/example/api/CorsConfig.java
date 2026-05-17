0be04a9c981bdae1575da6f975e1aaa7e0cc3112
Aman <avaman7860@gmail.com>
2026-05-17 16:36:31 +0530

    refactored

--- apiGateway/src/main/java/com/example/api/CorsConfig.java
+++ apiGateway/src/main/java/com/example/api/CorsConfig.java
@@ -14,7 +14,7 @@ public class CorsConfig {
     @Bean
     public CorsWebFilter corsWebFilter() {
         CorsConfiguration corsConfig = new CorsConfiguration();
-        corsConfig.setAllowedOrigins(Arrays.asList("http://localhost:5173", "http://localhost:5174"));
+        corsConfig.setAllowedOrigins(Arrays.asList("http://localhost:5173", "http://localhost:5174", "https://quick-bite-frontend-five.vercel.app"));
         corsConfig.setMaxAge(3600L);
         corsConfig.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"));
         corsConfig.setAllowedHeaders(Arrays.asList("Authorization", "Content-Type"));
